3dde801046bbbcb55f01a9cf0166b5a56aeaa1e8
AdamKh <adamkhashiev2004@mail.ru>
2022-03-15 21:25:02 +0300

    Delete CPP content

--- prog/ConsoleApplication1/ConsoleApplication1.cpp
+++ prog/ConsoleApplication1/ConsoleApplication1.cpp
@@ -1,18 +1,1 @@
-﻿#include <iostream>
-using namespace std;
-
-int main()
-{
-    cout << "Hello World!\n";
-    int a, b;
-    a = 5;
-    b = 2;
-    cout << "a = " << a << "b = " << b;
-    if (a < b) {
-        cout << "\na < b";
-    }
-    else
-    {
-        cout << "\nb < a";
-    }
-}
+﻿
